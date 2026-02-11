61e3dab1e2d8bc7cfb05412bb63bf08dbac52676
Samuel Hope <samuelhope805@gmail.com>
2026-02-11 14:04:33 +0300

    mobile hero section height fix-fixed navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/NavBar";
 import Newsletter from "./components/Newsletter";
 
-/*export default function Home() {
+export default function Home() {
   return (
     <div className="min-h-screen bg-black">
       <Navbar />
@@ -14,25 +14,6 @@ import Newsletter from "./components/Newsletter";
       <DesignerShowcase />
       <Newsletter />
       <Footer />
-    </div>
-  )
-}*/
-
-export default function Home() {
-  return (
-    <div className="bg-black">
-      <Navbar />
-      
-      {/* HeroSection now handles its own height */}
-      <HeroSection />
-
-      {/* Rest of the page */}
-      <div className="relative z-10">
-        <FeaturedProducts />
-        <DesignerShowcase />
-        <Newsletter />
-        <Footer />
-      </div>
     </div>
   )
 }
