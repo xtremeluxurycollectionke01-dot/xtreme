46b8b387e7b4ae5fd97b1cbd3bb972ccb58188e6
Samuel Hope <samuelhope805@gmail.com>
2026-06-04 17:54:21 +0300

    port fix

--- server.js
+++ server.js
@@ -499,7 +499,9 @@ app.prepare().then(async () => {
     });
   });
 
-  server.listen(3000, () => {
-    console.log("🚀 Server running on http://localhost:3000");
-  });
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 });
